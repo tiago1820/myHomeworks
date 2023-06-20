f179ab2e45b104d1da125a4fe525c7f11ead8404
tiago1820 <tiago.zdo@gmail.com>
2023-06-20 18:45:37 -0300

    Practicando JavaScript

---
+++ 0003.js
@@ -0,0 +1,39 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+    // your code here
+    if (input === null || input.length === 0) {
+        return [];
+    }
+
+    let count = 0;
+    let sum = 0;
+    let newArray = [];
+
+    for (let i = 0; i < input.length; i++) {
+        let element = input[i];
+        if (element > 0) {
+            count++;
+        } else if (element < 0) {
+            sum += element;
+        }
+
+    }
+
+    newArray.push(count);
+    newArray.push(sum);
+
+    return newArray;
+
+}
+
+// let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+let input = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
+console.log(countPositivesSumNegatives(input));
---
+++ 0004.js
@@ -0,0 +1,18 @@
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string){
+
+	// code code code
+    let newArray = string.split(" ");
+
+    return newArray;
+
+}
+
+let string = "I love arrays they are my favorite";
+console.log(stringToArray(string));
