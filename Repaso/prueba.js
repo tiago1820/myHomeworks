c577aed10b32f338e075987c6253af2ff59af045
tiago1820 <tiago.zdo@gmail.com>
2023-07-19 19:12:13 -0300

    El arbol

--- Repaso/prueba.js
+++ Repaso/prueba.js
@@ -1,80 +1,118 @@
-function Node(valor) {
-    this.valor = valor;
-    this.siguiente = null;
+function Barbol(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
 }
-
-function LinkedList() {
-    this.cabeza = null;
-}
-
-LinkedList.prototype.add = function (valor) {
-    let nuevoNodo = new Node(valor);
-
-    if (this.cabeza === null) {
-        this.cabeza = nuevoNodo;
-    } else {
-        let actual = this.cabeza;
-        while (actual.siguiente !== null) {
-            actual = actual.siguiente;
-        }
-        actual.siguiente = nuevoNodo;
-    }
-}
-
-//{valor:10,sig:{valor:15,sig:{valor:20,sig:{valor:25,sig:{valor:30,sig:null}}}}}
-LinkedList.prototype.remove = function () {
-    if (this.cabeza === null) {
-        return null;
-    } else if (this.cabeza.siguiente === null) {
-        let valorRemovido = this.cabeza.valor;
-        this.cabeza = null;
-        return valorRemovido;
-    } else {
-        let actual = this.cabeza;//{valor:10,sig:nodo}
-        let anterior = null;
-        while (actual.siguiente !== null) {
-            anterior = actual;//{valor:10,sig:nodo}
-            actual = actual.siguiente;//{valor:15,sig:null}
-        }
-
-        anterior.siguiente = null;//{valor:10,sig:null}
-        return actual.valor;//15
-    }
-}
-
-//{valor:10,sig:{valor:15,sig:{valor:20,sig:{valor:25,sig:{valor:30,sig:null}}}}}
-LinkedList.prototype.search = function (param) {//
-    if (this.cabeza === null) {
-        return null;
-    } else {
-        let actual = this.cabeza;//{valor:10,seg:nodo}
-        if (typeof param === 'function') {
-            while (actual !== null) {
-                if (param(actual.valor) === true) {//
-                    return actual.valor;
-                }
-                actual = actual.siguiente;//null
-            }
+//{value:10,left:{value:5,left:null,right:null},right:{value:15,left:{value:12,left:null,right:null},right:null}}
+Barbol.prototype.insert = function (value) {//12
+    if (value < this.value) {//12 < 15
+        if (!this.left) {
+            this.left = new Barbol(value);//{value:12,left:null,right:null}
         } else {
-            while (actual !== null) {
-                if (actual.valor === param) {
-                    return actual.valor;
-                }
-                actual = actual.siguiente;
-            }
+            this.left.insert(value);
         }
-        return null;
+    } else if (value > this.value) {//12 > 10
+        if (!this.right) {
+            this.right = new Barbol(value);//{value:15,left:null,right:null}
+        } else {
+            this.right.insert(value);//12
+        }
     }
 }
 
-function isEven(n){
-    return n % 2 === 0;
+Barbol.prototype.size = function () {
+    let count = 1;
+
+    if (this.left) {
+        count += this.left.size();
+    }
+
+    if (this.right) {
+        count += this.right.size();
+    }
+
+    return count;
 }
 
-let lista = new LinkedList();
-lista.add(10)
-lista.add(15)
-lista.add(20)
-lista.add(25)
+//{value:10,left:{value:5,left:null,right:null},right:{value:15,left:{value:12,left:null,right:null},right:null}}
+// 12
+Barbol.prototype.contains = function (value) {//12
+    if (this.value === value) {
+        return true;
+    } else if (value < this.value) {//
+        if (this.left) {
+            return this.left.contains(value);
+        } else {
+            return false;
+        }
+    } else {
+        if (this.right) {
+            return this.right.contains(value);
+        } else {
+            return false;
+        }
+    }
+}
 
-console.log(lista.search(25))
+//{value:10,left:{value:5,left:null,right:null},right:{value:15,left:{value:12,left:null,right:null},right:null}}
+Barbol.prototype.depthFirstForEach = function (order = "in-order", callback) {//post-order
+    function preOrder(node, callback) {//
+        if (node) {
+            callback(node.value);
+            preOrder(node.left, callback);
+            preOrder(node.right, callback);
+        }
+    }
+
+    function inOrder(node, callback) {//
+        if (node) {
+            inOrder(node.left, callback);
+            callback(node.value);
+            inOrder(node.right, callback);
+        }
+    }
+
+    function postOrder(node, callback) {//5 12 15 10
+        if (node) {
+            postOrder(node.left, callback);//
+            postOrder(node.right, callback);
+            callback(node.value);
+        }
+    }
+
+    if (order === "pre-order") {
+        preOrder(this, callback);
+    } else if (order === "in-order") {
+        inOrder(this, callback);
+    } else if (order === "post-order") {
+        postOrder(this, callback);
+    }
+
+}
+
+Barbol.prototype.breadthFirstForEach = function (callback) {
+    const queue = [this];//
+
+    while (queue.length > 0) {
+        const node = queue.shift();//
+        callback(node.value);
+        if (node.left) {
+            queue.push(node.left);
+        }
+        if (node.right) {
+            queue.push(node.right);
+        }
+    }
+}
+
+function saludar(valor) {
+    console.log("Hola");
+}
+
+let arbol = new Barbol(10);
+arbol.insert(15)
+arbol.insert(5)
+arbol.insert(12)
+arbol.insert(2)
+
+arbol.breadthFirstForEach(saludar)
