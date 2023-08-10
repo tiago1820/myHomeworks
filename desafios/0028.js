4256aa3587966b818befdfa00c81be2c84d95b39
tiago1820 <tiago.zdo@gmail.com>
2023-08-09 23:56:49 -0300

    To convert boolean values to their corresponding string representations.

---
+++ desafios/0028.js
@@ -0,0 +1,7 @@
+//Para convertir valores booleanos en sus representaciones de cadena correspondientes.
+function booleanToString(b){
+    return b.toString();
+}
+
+console.log(booleanToString(true));
+console.log(booleanToString(false))
